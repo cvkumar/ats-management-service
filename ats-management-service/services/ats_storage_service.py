514b1ce652c741117dc807e3be5120991c4358d0
calebkumar <caleb.kumar@hiremya.com>
2020-02-04 17:18:27 -0800

    handle tenant by key not by table

--- ats-management-service/services/ats_storage_service.py
+++ ats-management-service/services/ats_storage_service.py
@@ -1,21 +1,36 @@
 import settings
+from common.constants import ATS_TABLE
+
+
+def _check_and_set_tenant(config, tenant):
+    if not config.get("tenant"):
+        config["tenant"] = tenant
+    return config
 
 
 def insert_ats_config(tenant: str, config: dict):
-    db_result = settings.DATABASE[tenant].insert_one(config)
+    config = _check_and_set_tenant(config, tenant)
+    db_result = settings.DATABASE[ATS_TABLE].insert_one(config)
     return db_result
 
 
 def retrieve_ats_config(tenant: str):
-    db_result = settings.DATABASE[tenant].find_one()
+    db_result = settings.DATABASE[ATS_TABLE].find_one(
+        {"tenant": tenant}, sort=[("_id", -1)]
+    )
     return db_result
 
 
-def delete_ats_config_many(tenant: str):
-    result = settings.DATABASE[tenant].delete_many({})
+def delete_ats_config(tenant: str):
+    result = settings.DATABASE[ATS_TABLE].delete_one({"tenant": tenant})
     return result
 
 
-def delete_ats_config(tenant: str):
-    result = settings.DATABASE[tenant].delete_one({})
+def delete_ats_config_many(tenant: str):
+    result = settings.DATABASE[ATS_TABLE].delete_many({"tenant": tenant})
+    return result
+
+
+def delete_ats_config_all(tenant: str):
+    result = settings.DATABASE[ATS_TABLE].delete_many({})
     return result

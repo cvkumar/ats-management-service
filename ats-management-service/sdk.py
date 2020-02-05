fad5edb84227cde47ed7dc879e11cb822752ce7b
calebkumar <caleb.kumar@hiremya.com>
2020-02-05 14:20:31 -0800

    Add beginning sdk code

--- ats-management-service/sdk.py
+++ ats-management-service/sdk.py
@@ -1,6 +1,16 @@
-# class AtsManagementClient():
-#     def __init__(self, tenant, ):
-#         self.tenant = tenant
-#
-#     def get_ats_config(self, tenant):
-#
+import requests
+
+
+class AtsManagementClient:
+    def __init__(self, tenant, url):
+        self.tenant = tenant
+        self.url = url
+        self.ats_url = f"{url}/{tenant}/ats"
+
+    def get_ats_config(self):
+        response = requests.get(self.ats_url)
+        return response
+
+    def post_ats_config(self, payload):
+        response = requests.post(self.ats_url, json=payload)
+        return response
